fb7e8b9bc9f5596192b4fdcf818e23b01a63d530
Mario_junior23 <mariojunior3251@gmail.com>
2020-12-04 05:07:58 -0300

    Adicionando e revendo conceitos de Modelos de atributos e repassando valores

--- src/main/java/com/project/nasaAPI/Controller/HomeController.java
+++ src/main/java/com/project/nasaAPI/Controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.project.nasaAPI.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
    
 	@GetMapping("/")
-	public String getHomeView() {
+	public String getHomeView(ModelMap model) {
+		model.put("name","page");
+		model.put("addres","123 fake st.");
+		return "index";
+	}
+	
+	@GetMapping("/testing")
+	public String getHomeTesteView(ModelMap model) {
+		model.put("name", "Jhon Doe");
 		return "index";
 	}
 }
