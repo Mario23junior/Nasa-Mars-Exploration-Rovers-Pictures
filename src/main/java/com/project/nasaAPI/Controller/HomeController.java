e6522aeed49edf451a54a259d29bd96f7f2d4b81
Mario_junior23 <mariojunior3251@gmail.com>
2020-12-05 04:35:15 -0300

    Alterando dados e adicionando service de mapeamento do objeto e adicioanando dependencia

--- src/main/java/com/project/nasaAPI/Controller/HomeController.java
+++ src/main/java/com/project/nasaAPI/Controller/HomeController.java
@@ -4,8 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.project.nasaAPI.Service.MarsRoverApiService;
+
 @Controller
 public class HomeController {
+	
+	private MarsRoverApiService roverService;
    
 	@GetMapping("/")
 	public String getHomeView(ModelMap model) {
--- src/main/java/com/project/nasaAPI/Service/MarsRoverApiService.java
+++ src/main/java/com/project/nasaAPI/Service/MarsRoverApiService.java
@@ -1,8 +1,19 @@
 package com.project.nasaAPI.Service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.project.nasaAPI.Response.MarsRoverApiResponse;
 
 @Service
 public class MarsRoverApiService {
-
+   
+	public MarsRoverApiResponse getRoverDate() {
+	RestTemplate rt = new RestTemplate();
+		
+		String url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=MEaxR5xw5lVBshWdtopSRyEsiUKSQ7rEPt9YD8S2";
+	    ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity(url, MarsRoverApiResponse.class);
+        return (response.getBody());
+	}
 }
