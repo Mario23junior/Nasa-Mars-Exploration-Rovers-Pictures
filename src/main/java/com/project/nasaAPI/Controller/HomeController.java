78aaf9900561bfbaafd6d5a33631956ebc0c090b
Mario_junior23 <mariojunior3251@gmail.com>
2020-12-05 04:43:12 -0300

    Adicionando dependencia service

--- src/main/java/com/project/nasaAPI/Controller/HomeController.java
+++ src/main/java/com/project/nasaAPI/Controller/HomeController.java
@@ -4,15 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.project.nasaAPI.Response.MarsRoverApiResponse;
 import com.project.nasaAPI.Service.MarsRoverApiService;
 
 @Controller
 public class HomeController {
 	
 	private MarsRoverApiService roverService;
-   
+	
+	public HomeController(MarsRoverApiService roverService) {
+		this.roverService = roverService;
+	 }
+	   
 	@GetMapping("/")
 	public String getHomeView(ModelMap model) {
+		MarsRoverApiResponse roverData = roverService.getRoverDate();
   		return "index";
 	}
 }
