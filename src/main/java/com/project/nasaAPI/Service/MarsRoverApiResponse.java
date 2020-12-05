4f9876dcf9e4ee6d55684f523ff59c634bcae077
Mario_junior23 <mariojunior3251@gmail.com>
2020-12-05 00:56:28 -0300

    Criando metodos acessores para captura dos dados recebidos no ArrayList

--- src/main/java/com/project/nasaAPI/Service/MarsRoverApiResponse.java
+++ src/main/java/com/project/nasaAPI/Service/MarsRoverApiResponse.java
@@ -6,12 +6,19 @@ import java.util.List;
 import com.project.nasaAPI.Model.MarsPhotoModel;
 
 public class MarsRoverApiResponse {
-   List<MarsPhotoModel> photo = new ArrayList<>();
+   List<MarsPhotoModel> photos = new ArrayList<>();
 
-@Override
-public String toString() {
-	return "MarsRoverApiResponse [photo=" + photo + "]";
-}
-   
-   
+	@Override
+	public String toString() {
+		return "MarsRoverApiResponse [photo=" + photos + "]";
+	}
+
+	public List<MarsPhotoModel> getPhotos() {
+		return photos;
+	}
+
+	public void setPhoto(List<MarsPhotoModel> photos) {
+		this.photos = photos;
+	}
+  
 }
