501cd12402699c275986de13ab27ad4ceafa3f1b
Mario_junior23 <mariojunior3251@gmail.com>
2020-12-06 07:26:08 -0300

    Adicionando acao de elementos dinamicos

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,9 @@
-document.getElementById("curiosity").addEventListener("click", function(){
-     alert("Óla eu sou o curiosity");
-})
+ let marsApiButtons = document.querySelectorAll("button[id *= 'marsApi']")
+ 
+ marsApiButtons.forEach(button => button.addEventListener('click', function () {
+                     const buttonId = this.id
+                     const roverId = buttonId.split("marsApi")[1]
+                     alert(roverId)
+                 }))
+ 
+ 
