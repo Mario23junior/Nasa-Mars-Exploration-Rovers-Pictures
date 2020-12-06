5cc955cc1cc541ce09f8ae13a7a8bf453d9b2b40
Mario_junior23 <mariojunior3251@gmail.com>
2020-12-06 04:34:19 -0300

    Renderizando imagens de forma dinamica dentro de um ForEach

--- src/main/java/com/project/nasaAPI/Service/MarsRoverApiService.java
+++ src/main/java/com/project/nasaAPI/Service/MarsRoverApiService.java
@@ -12,7 +12,7 @@ public class MarsRoverApiService {
 	public MarsRoverApiResponse getRoverDate() {
 	RestTemplate rt = new RestTemplate();
 		
-		String url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=MEaxR5xw5lVBshWdtopSRyEsiUKSQ7rEPt9YD8S2";
+		String url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2&api_key=MEaxR5xw5lVBshWdtopSRyEsiUKSQ7rEPt9YD8S2";
 	    ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity(url, MarsRoverApiResponse.class);
         return (response.getBody());
 	}
