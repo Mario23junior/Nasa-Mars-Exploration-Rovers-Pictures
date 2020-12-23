0fa1354c0a4d9341c127f8c2dd99601534d42e6d
Mario23junior <mariojunior3251@gmail.com>
2020-12-23 15:37:23 -0300

    Adicionando cameras de captura da sonda em cemeras especificas

--- src/main/java/com/project/nasaAPI/DTO/HomeDto.java
+++ src/main/java/com/project/nasaAPI/DTO/HomeDto.java
@@ -4,11 +4,124 @@ public class HomeDto {
     
 	private String marsApiRoverData;
 	private Integer marsSol;
+	
 	private Boolean defaultCheck1;
+	private Boolean cameraFhaz;	
+	private Boolean cameraRhaz;
+	private Boolean cameraMast;
+	private Boolean cameraChemcam;
+	private Boolean cameraMahli;
+	private Boolean cameraMardi;
+	private Boolean cameraNavcam;
+	private Boolean cameraPancam;
+	private Boolean cameraMinites;
+		
 	
 	public HomeDto() {
  	}
-		
+	
+	public Boolean getCameraFhaz() {
+		return cameraFhaz;
+	}
+
+	public void setCameraFhaz(Boolean cameraFhaz) {
+		this.cameraFhaz = cameraFhaz;
+	}
+
+	public Boolean getCameraRhaz() {
+		return cameraRhaz;
+	}
+
+	public void setCameraRhaz(Boolean cameraRhaz) {
+		this.cameraRhaz = cameraRhaz;
+	}
+
+
+
+	public Boolean getCameraMast() {
+		return cameraMast;
+	}
+
+
+
+	public void setCameraMast(Boolean cameraMast) {
+		this.cameraMast = cameraMast;
+	}
+
+
+
+	public Boolean getCameraChemcam() {
+		return cameraChemcam;
+	}
+
+
+
+	public void setCameraChemcam(Boolean cameraChemcam) {
+		this.cameraChemcam = cameraChemcam;
+	}
+
+
+
+	public Boolean getCameraMahli() {
+		return cameraMahli;
+	}
+
+
+
+	public void setCameraMahli(Boolean cameraMahli) {
+		this.cameraMahli = cameraMahli;
+	}
+
+
+
+	public Boolean getCameraMardi() {
+		return cameraMardi;
+	}
+
+
+
+	public void setCameraMardi(Boolean cameraMardi) {
+		this.cameraMardi = cameraMardi;
+	}
+
+
+
+	public Boolean getCameraNavcam() {
+		return cameraNavcam;
+	}
+
+
+
+	public void setCameraNavcam(Boolean cameraNavcam) {
+		this.cameraNavcam = cameraNavcam;
+	}
+
+
+
+	public Boolean getCameraPancam() {
+		return cameraPancam;
+	}
+
+
+
+	public void setCameraPancam(Boolean cameraPancam) {
+		this.cameraPancam = cameraPancam;
+	}
+
+
+
+	public Boolean getCameraMinites() {
+		return cameraMinites;
+	}
+
+
+
+	public void setCameraMinites(Boolean cameraMinites) {
+		this.cameraMinites = cameraMinites;
+	}
+
+
+
 	public String getMarsApiRoverData() {
 		return marsApiRoverData;
 	}
