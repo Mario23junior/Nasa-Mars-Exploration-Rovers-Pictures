fad48e62528139549bd44807786c59bb73950041
Mario23junior <mariojunior3251@gmail.com>
2020-12-22 23:37:19 -0300

    realce de botoes com javascript ao presionar sonda e capturando parametro

--- src/main/java/com/project/nasaAPI/Controller/HomeController.java
+++ src/main/java/com/project/nasaAPI/Controller/HomeController.java
@@ -2,13 +2,8 @@ package com.project.nasaAPI.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-<<<<<<< HEAD
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-=======
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
->>>>>>> 3f6cc4ec4062510c8d6f936fe3298abc0274c605
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.nasaAPI.Response.MarsRoverApiResponse;
@@ -25,29 +20,12 @@ public class HomeController {
 	 }
 	   
 	@GetMapping("/")
-<<<<<<< HEAD
-	public String getHomeView(ModelMap model, @RequestParam(required = false) String marsApiRoverData) {
-		if( StringUtils.isEmpty(marsApiRoverData)) {
+ 	public String getHomeView(ModelMap model, @RequestParam(required = false) String marsApiRoverData) {
+		if(StringUtils.isEmpty(marsApiRoverData)) {
 			marsApiRoverData = "opportunity";
 		}
 		MarsRoverApiResponse roverData = roverService.getRoverDate(marsApiRoverData);
-=======
-	public String getHomeView(ModelMap model) {
-		MarsRoverApiResponse roverData = roverService.getRoverDate("curiosity");
->>>>>>> 3f6cc4ec4062510c8d6f936fe3298abc0274c605
-		model.put("roverData", roverData);
+   		model.put("roverData", roverData);
   		return "index";
 	}
-	
-<<<<<<< HEAD
- 
-=======
-	@PostMapping("/")
-	public String postHomeView(ModelMap model , @RequestParam String marsApiRoverData) {
-		 MarsRoverApiResponse roverData = roverService.getRoverDate(marsApiRoverData);
-		 model.put("roverData",roverData);
-		 
-		 return "index";
-	}
->>>>>>> 3f6cc4ec4062510c8d6f936fe3298abc0274c605
-}
+ }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -7,5 +7,27 @@
                      apiData.value = roverId
                      document.getElementById("frmRoverType").submit()
                   }))
- 
+  
+	function getUrlParameter(name) {
+	    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+	    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+	    var results = regex.exec(location.search);
+	    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+	}
+	
+	let marsRoverType = getUrlParameter("marsApiRoverData");
+	highlightBtnRoverType(marsRoverType) 
+	function highlightBtnRoverType (roverType) {
+		
+		 if (marsRoverType == '') {
+         marsRoverType = 'opportunity' 
+    }
+		
+		document.getElementById('marsApi' + roverType).classList.remove('btn-secondary')
+		document.getElementById('marsApi' + roverType).classList.add('btn-primary')
+	}
+
+
+
+
  
